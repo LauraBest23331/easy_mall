0b910e52afd262d5718b729e83fbe734d1b86a9b
ZhangYang <18111002318@163.com>
2020-10-30 10:36:51 +0800

    create dir

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
